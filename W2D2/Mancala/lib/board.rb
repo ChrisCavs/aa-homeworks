910059be56beb5e26573ea3ec848359ceb45687f
Chris <chris211321@gmail.com>
2018-05-28 21:10:26 -0400

    Mancala

---
+++ W2D2/Mancala/lib/board.rb
@@ -0,0 +1,88 @@
+require 'byebug'
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+     @player1 = name1
+     @player2 = name2
+     @cups = Array.new(14) { Array.new }
+     place_stones
+  end
+
+  def place_stones
+    cups.each_with_index do |cup, idx|
+      next if idx == 6 || idx == 13
+      4.times do
+        cup << :stone
+      end
+    end
+  end
+
+  def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos > 14 || start_pos <= 0
+  end
+
+  def make_move(start_pos, current_player_name)
+    # empties cup
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+
+    # distributes stones
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+      # places stones in the correct current player's cups
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @player1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @player2
+      else
+        @cups[cup_idx] << stones.pop
+      end
+    end
+
+    render
+    next_turn(cup_idx)
+  end
+
+  def next_turn(ending_cup_idx)
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      # ended on store -- get to choose where to start again
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      # ended on empty cup -- switches players' turns
+      :switch
+    else
+      # ended on cup with stones in it -- automatically starts there
+      ending_cup_idx
+    end
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+    @cups.take(6).all? { |cup| cup.empty? } ||
+    @cups[7..12].all? { |cup| cup.empty? }
+  end
+
+  def winner
+    player1_count = @cups[6].count
+    player2_count = @cups[13].count
+
+    if player1_count == player2_count
+      :draw
+    else
+      player1_count > player2_count ? @player1 : @player2
+    end
+  end
+
+  private
+  attr_reader :player1, :player2
+end
